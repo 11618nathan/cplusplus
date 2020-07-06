d8d5e3061bef1b80cc227acc6620ddd72c240d77
Baek Gi Chan <11618nathan@gmail.com>
2020-07-06 22:11:37 +0900

    Create

---
+++ stdcout.cpp
@@ -0,0 +1,14 @@
+
+// c++ 출력
+#include <iostream>
+
+int main(void)
+{
+	int num=20;
+	std::cout<<"Hello world!"<<std::endl;
+
+	// std::cout<< '출력대상';
+	// 별도의 출력포맷을 지정x
+	// <<연산자 - 둘 이상 출력을 명령
+	// std::endl - 개행
+}
