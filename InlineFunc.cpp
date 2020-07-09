722769d1a4cc736f51b03171fecbeb819bf7c53b
Baek Gi Chan <11618nathan@gmail.com>
2020-07-09 23:05:31 +0900

    create

--- InlineFunc.cpp
+++ InlineFunc.cpp
@@ -1,18 +1,13 @@
-
 #include <iostream>
 
-inline int square(int num)
+inline int SQUARE(int x)
 {
-	return num * num;
+	return x*x;
 }
 
 int main(void)
 {
-	std::cout << square(5) << std::endl;
-	std::cout << square(12) << std::endl;
+	std::cout<<SQUARE(5)<<std::endl;
+	std::cout<<SQUARE(12)<<std::endl;
 	return 0;
 }
-
-// 인라인 함수 - 매크로 함수는 정의하기가 복잡, 일반 함수처럼 정의
-// inline을 이용한 함수의 인라인화는 컴파일러에 의해서 처리
-// 매크로를 이용한 함수의 인라인화는 전처리기에 의해 처리.
